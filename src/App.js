9a22dbed22be16b776bc337088464aede9cc4e66
Andrew Chalkley <andrew@teamtreehouse.com>
2017-07-17 13:02:54 -0700

    Added structure for header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
 import CourseList from "./components/CourseList";
+import Header from "./components/Header";
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
-          <h1>Helloworld</h1>
+          <Header />
           <CourseList />
         </div>
       </Provider>
---
+++ src/components/Header/Navigation.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Navigation = props => <ul>
+    <li>Nav 1</li>
+    <li>Nav 2</li>
+</ul>;
+
+export default Navigation;
