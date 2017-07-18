2af6460a99f929e036c3fd695a0a2c71acee09a3
Joel Kraft <joelkraft3@yahoo.com>
2017-07-18 12:24:47 -0700

    complete the basic course reducers

--- src/reducers/course.js
+++ src/reducers/course.js
@@ -2,10 +2,34 @@ import { courseActions } from "../actionTypes";
 
 export default (state = [], action) => {
   switch (action.type) {
+    case courseActions.REQUEST_COURSE:
+      return state;
     case courseActions.REQUEST_COURSE_SUCCESS:
       return action.course;
     case courseActions.REQUEST_COURSE_FAILURE:
       return state;
+
+    case courseActions.CREATE_COURSE:
+      return state;
+    case courseActions.CREATE_COURSE_SUCCESS:
+      return action.course;
+    case courseActions.CREATE_COURSE_FAILURE:
+      return state;
+
+    case courseActions.EDIT_COURSE:
+      return state;
+    case courseActions.EDIT_COURSE_SUCCESS:
+      return action.course;
+    case courseActions.EDIT_COURSE_FAILURE:
+      return state;
+
+    case courseActions.CREATE_REVIEW:
+      return state;
+    case courseActions.CREATE_REVIEW_SUCCESS:
+      return action.review;
+    case courseActions.CREATE_REVIEW_FAILURE:
+      return state;
+
     default:
       return state;
   }
