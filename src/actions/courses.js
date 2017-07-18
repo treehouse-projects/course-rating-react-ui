5ad181651f42103dcb3fe263404eafdfeadff2ef
Nicolas Hampton <nicolashampton@Nicolass-MacBook-Pro-2.local>
2017-07-17 21:52:08 -0700

    catch up commit

--- src/actions/courses.js
+++ src/actions/courses.js
@@ -1,6 +1,7 @@
 import fetch from 'isomorphic-fetch'
+import { apiRoot } from "../config";
 
-import * as courseActions from '../actionTypes';
+import * as courseActions from '../actionTypes/courses';
 
 const courseState = 
   [
@@ -19,20 +20,34 @@ const courseState =
   ]
 ;
 
-export function listCourses() {
+export function requestCourseListSuccess(data) {
   return {
-    type: courseActions.REQUEST_COURSE_LIST
-  }
+    type: courseActions.REQUEST_COURSE_LIST_SUCCESS,
+    courses: data
+  };
 }
-export function listCourses() {
+export function requestCourseListFailure(err) {
   return {
-    type: courseActions.LIST_COURSES,
-    courseList: [...courseState]
-  }
+    type: courseActions.REQUEST_COURSE_LIST_FAILURE,
+    err: err
+  };
 }
-export function listCourses() {
-  return {
-    type: courseActions.LIST_COURSES,
-    courseList: [...courseState]
-  }
+
+export function fetchCourseList() {
+  return dispatch => {
+    return fetch(`${apiRoot}/courses`, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(({ data }) => {
+        dispatch(requestCourseListSuccess(data));
+      })
+      .catch(err => {
+        dispatch(requestCourseListFailure());
+        console.log(err);
+      });
+  };
 }
+
--- src/store.js
+++ src/store.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "development") {
   }
 }
 
-const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+const composedEnhancers = compose(applyMiddleware(...middleware));
 
 const store = createStore(rootReducer, initialState, composedEnhancers);
 
