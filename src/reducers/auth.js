654a79ee714750cbf0b148c731e6f0c5feb2ff17
Andrew Chalkley <andrew@teamtreehouse.com>
2017-07-19 09:49:36 -0700

    Added cookie logic

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,8 +1,23 @@
 import { authActions } from "../actionTypes";
+import Cookies from "js-cookie";
 
-export default (state = null, action) => {
+function getAuthenticationHeader() {
+  const authentication = Cookies.get("authentication");
+  if (authentication) {
+    return authentication;
+  } else {
+    return null;
+  }
+}
+
+function setAuthenticationHeader(authentication) {
+  Cookies.set("authentication", authentication);
+}
+
+export default (state = getAuthenticationHeader(), action) => {
   switch (action.type) {
     case authActions.USER_AUTHENTICATED:
+      setAuthenticationHeader(action.authentication);
       return action.authentication;
     case authActions.LOG_OUT_USER:
       return null;
