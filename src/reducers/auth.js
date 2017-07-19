99ff1a27649f75aef962bad6b73e98c40da88a96
Andrew Chalkley <andrew@teamtreehouse.com>
2017-07-19 09:26:46 -0700

    Added authentication action

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,6 +2,8 @@ import { authActions } from "../actionTypes";
 
 export default (state = null, action) => {
   switch (action.type) {
+    case authActions.USER_AUTHENTICATED:
+      return action.authentication;
     case authActions.LOG_OUT_USER:
       return null;
     default:
