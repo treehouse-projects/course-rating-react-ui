3a351e795b8f72c404859cff42620e2775c2eafb
Joel Kraft <joelkraft3@yahoo.com>
2017-07-19 10:47:17 -0700

    create form handlers added

--- src/containers/CreateCourse.js
+++ src/containers/CreateCourse.js
@@ -4,13 +4,34 @@ import { connect } from "react-redux";
 import { courseActions } from "../actions";
 
 class CreateCourse extends Component {
-
   componentDidMount() {
-    this.props.onMount(this.props.match.params.id);
+    // this.props.onMount(this.props.match.params.id);
   }
 
   state = {
-    steps: [{ }]
+    steps: [{}]
+  };
+
+  changeHandler(formField, value) {
+    this.setState({
+      ...this.state,
+      [formField]: value
+    });
+    console.log(this.state);
+  }
+
+  stepChangeHandler(stepField, value, index) {
+    this.setState({
+      ...this.state,
+      steps: this.state.steps.map((step, i) => {
+        if (i === index) {
+          return {
+            ...this.state.steps[i],
+            [stepField]: value
+          };
+        }
+      })
+    });
   }
 
   addStep(index) {
@@ -28,8 +49,8 @@ class CreateCourse extends Component {
     // insert the new step
     steps.splice(index, 0, {
       stepNumber: newStepNumber,
-      title: '',
-      description: ''
+      title: "",
+      description: ""
     });
     this.setState({ steps: this.state.steps });
   }
@@ -56,46 +77,103 @@ class CreateCourse extends Component {
         <div className="actions--bar">
           <div className="bounds">
             <div className="grid-100">
-              <a className="button" href="#">Create Course</a>
+              <a className="button" href="#">
+                Create Course
+              </a>
             </div>
           </div>
         </div>
 
         <div className="bounds course--detail">
-
+          <div>
+            <h2 className="validation--errors--label">Validation errors</h2>
+            <div className="validation-errors">
+              <ul>
+                <li className="ng-binding ng-scope">description is required</li>
+                <li className="ng-binding ng-scope">title is required</li>
+                <li className="ng-binding ng-scope">
+                  Step requires a description
+                </li>
+                <li className="ng-binding ng-scope">Step requires a title</li>
+              </ul>
+            </div>
+          </div>
           <div className="grid-66">
             <div className="course--header">
               <h4 className="course--label">Course</h4>
-              <input type="text" placeholder="Course Title..." className="input-title course--title--input" value={ this.props.course.title} />
+              <input
+                type="text"
+                placeholder="Course Title..."
+                className="input-title course--title--input"
+                onChange={e => {
+                  this.changeHandler("courseTitle", e.target.value);
+                }}
+                value={this.state.courseTitle}
+              />
               <p>By </p>
             </div>
             <div className="course--description">
-              <textarea className="autogrow" placeholder="Course description..."></textarea>
+              <textarea
+                className="autogrow"
+                placeholder="Course description..."
+                onChange={e => {
+                  this.changeHandler("courseDescription", e.target.value);
+                }}
+                value={this.state.courseDescription}
+              />
               <div className="course--steps">
                 <h2>Add steps</h2>
                 {/* STEPS */}
                 <ol>
                   {this.state.steps.map((step, i) => {
                     return (
-                      <li>
+                      <li key={i}>
                         <h3>
-                          { this.props.course.stepNumber } <input type="text" placeholder="Step Title..." className="input-title course--steps--input"/>
+                          {this.props.course.stepNumber}{" "}
+                          <input
+                            type="text"
+                            placeholder="Step Title..."
+                            className="input-title course--steps--input"
+                            onChange={e => {
+                              this.stepChangeHandler(
+                                "stepTitle",
+                                e.target.value,
+                                i
+                              );
+                            }}
+                            value={this.state.steps[i].stepTitle}
+                          />
                         </h3>
-                        <textarea placeholder="Step description..." className="autogrow"></textarea>
-                        <a className="course--steps--add" onClick={() => this.addStep(i)}>
-                          <svg x="0px" y="0px" viewBox="0 0 13 13" className="add">
-                            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "/>
+                        <textarea
+                          placeholder="Step description..."
+                          className="autogrow"
+                        />
+                        <a
+                          className="course--steps--add"
+                          onClick={() => this.addStep(i)}
+                        >
+                          <svg
+                            x="0px"
+                            y="0px"
+                            viewBox="0 0 13 13"
+                            className="add"
+                          >
+                            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
                           </svg>
                           Add step...
                         </a>
-                        <a className="course--steps--remove" onClick={() => this.removeStep(i)}>Remove</a>
+                        <a
+                          className="course--steps--remove"
+                          onClick={() => this.removeStep(i)}
+                        >
+                          Remove
+                        </a>
                       </li>
                     );
                   })}
                 </ol>
               </div>
               <a className="button">Save Course</a>
-
             </div>
           </div>
 
@@ -104,18 +182,29 @@ class CreateCourse extends Component {
               <ul className="course--stats--list">
                 <li className="course--stats--list--item">
                   <h4>Estimated Time</h4>
-                  <input type="text" placeholder="Hours" />
+                  <input
+                    type="text"
+                    placeholder="Hours"
+                    onChange={e => {
+                      this.changeHandler("hours", e.target.value);
+                    }}
+                    value={this.state.hours}
+                  />
                 </li>
                 <li className="course--stats--list--item">
                   <h4>Materials Needed</h4>
-                  <textarea placeholder="List materials..."></textarea>
+                  <textarea
+                    placeholder="List materials..."
+                    onChange={e => {
+                      this.changeHandler("materials", e.target.value);
+                    }}
+                    value={this.state.materials}
+                  />
                 </li>
               </ul>
             </div>
           </div>
-
         </div>
-
       </div>
     );
   }
@@ -123,8 +212,4 @@ class CreateCourse extends Component {
 
 const mapStateToProps = state => ({ ...state });
 
-const mapDispatchToProps = dispatch => ({
-  onMount: id => dispatch(courseActions.fetchCourse(id))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CreateCourse);
+export default connect(mapStateToProps)(CreateCourse);
