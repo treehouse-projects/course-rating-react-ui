c6bd4a994ec1e8de7e719166901713c30e5a4a3c
Andrew Chalkley <andrew@teamtreehouse.com>
2017-07-20 13:51:50 -0700

    Hey, let's not unwrap everything

--- src/components/Header/BreadcrumbTitle.js
+++ src/components/Header/BreadcrumbTitle.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
 import { connect } from "react-redux";
 
 const BreadcrumbTitle = props => {
   return (
-      <span className="selected">{ props.breadcrumb }</span>
-  )
+    <span className="selected">
+      {props.breadcrumb}
+    </span>
+  );
 };
-const mapStateToProps = state => ({...state});
+const mapStateToProps = state => ({ breadcrumb: state.breadcrumb });
 
 export default connect(mapStateToProps)(BreadcrumbTitle);
